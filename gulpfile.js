0993a8e4a73fd55deb0fc9de75831a71ea8f3038
Syed Ekram Uddin Emon <ekram.syed@gmail.com>
2018-02-06 07:54:22 +0600

    postcss plugin added for autoprefixer support

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     autoprefixer = require('autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
+    postcss = require('gulp-postcss'),
     concat = require('gulp-concat');
 
 
@@ -12,9 +13,12 @@ gulp.task('sass', function() {
     return gulp.src(['src/scss/**/*.scss'])
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version'))
+        //.pipe(autoprefixer('last 2 version'))
+        .pipe(postcss([ autoprefixer()]))
         .pipe(concat('main.css'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest("dist/site/css"))
         .pipe(browserSync.stream());
 });
+
+gulp.task('default', ['sass']);
